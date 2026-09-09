787215a5b44fcabf02acebc6cea287e6f0679cdd
apenno75 <apenno75@gmail.com>
2026-09-09 15:17:40 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
   // La funzione edge che riconosce titolo/tipo dalla copertina.
   // Dopo il deploy avrà questa forma (nota: .supabase.co/functions/v1/..., non .functions.supabase.co):
   // https://TUO-PROGETTO.supabase.co/functions/v1/identify-cover
-  IDENTIFY_FUNCTION_URL: "https://apenno75.github.io/Filmografia//functions/v1/identify-cover",
+  IDENTIFY_FUNCTION_URL: "https://erukzvrtjnvyzjvfuirn.supabase.co/functions/v1/identify-cover",
 
   // Chiave gratuita da themoviedb.org (Impostazioni > API)
   TMDB_API_KEY: "2c669816245012266d15d06607d3d413",
